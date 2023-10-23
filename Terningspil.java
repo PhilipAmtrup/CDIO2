28d656d5c884a548a319f05f8b0ccad4b793346f
AsgerFrandsen <asgerfrandsen@gmail.com>
2023-10-23 14:59:49 +0200

    ¨Dice¨

--- Terningspil.java
+++ Terningspil.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Terningspil {
     public class Spiller1{
 
@@ -15,4 +17,16 @@ public class Terningspil {
 
         
     }
+
+    public class Dice{
+        Random rand = new Random();
+
+        int die1 = rand.nextInt(6) + 1;
+
+        int die2 = rand.nextInt(6) + 1;
+
+        int total = die1 + die2;
+
+    }
+
 }
