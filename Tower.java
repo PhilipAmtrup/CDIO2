423ab9ca61b6c8d7bc889667f1456ad5c1013dbe
Mo Zaabat <mzaabat@hotmail.com>
2023-10-23 15:24:40 +0200

    Tower

--- Tower.java
+++ Tower.java
@@ -1,3 +1,21 @@
 public class Tower {
+    if (total = 2){
+        bankkonto = 250 + bankkonto;
+    }
+
+    if (Turn == 0){
+        if (total == 2){
+            bankkonto1 = 250 + bankkonto1;
+        }
+        System.out.println("Du møder en troldmand han fremtryller en blå safir som du sælger for 250 penge. Din saldo er nu: " + bankkonto1); 
+        
+    } else Turn = 1;
+
+    if (Turn == 1){
+        if (total == 2){
+            bankkonto2 = 250 + bankkonto2;
+        }
+        System.out.println("Du møder en troldmand han fremtryller en blå safir som du sælger for 250 penge. Din saldo er nu: " + bankkonto2);
+    }else  Turn = 0;
     
 }
