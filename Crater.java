1067761df7c72370bf3cc8aa7fd4e54a60cbcc44
Mo Zaabat <mzaabat@hotmail.com>
2023-10-23 15:29:43 +0200

    crater

--- Crater.java
+++ Crater.java
@@ -4,18 +4,18 @@ public class Crater {
     
 
         if (Turn == 0){
-            if (total == 2){
-                bankkonto1 = 250 + bankkonto1;
+            if (total == 3){
+                bankkonto1 = 100 - bankkonto1;
             }
-            System.out.println("Du møder en troldmand han fremtryller en blå safir som du sælger for 250 penge. Din saldo er nu: " + bankkonto1); 
+            System.out.println("Du går i gennem et krater og brænder dine fødder på varmt rumstøv du betaler 100 penge for at få behandling. Din saldo er nu: " + bankkonto1); 
             
         } else Turn = 1;
 
         if (Turn == 1){
-            if (total == 2){
-                bankkonto2 = 250 + bankkonto2;
+            if (total == 3){
+                bankkonto2 = 100 - bankkonto2;
             }
-            System.out.println("Du møder en troldmand han fremtryller en blå safir som du sælger for 250 penge. Din saldo er nu: " + bankkonto2);
+            System.out.println("Du går i gennem et krater og brænder dine fødder på varmt rumstøv du betaler 100 penge for at få behandling. Din saldo er nu: " + bankkonto2);
         }else  Turn = 0;
     }
 }
