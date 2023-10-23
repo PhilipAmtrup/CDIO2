63033969ce95e0fd948d8a3f13c817de60d39533
Julius Østerbye <julius310502@gmail.com>
2023-10-23 15:47:03 +0200

    goldmine done

--- Goldmine.java
+++ Goldmine.java
@@ -1,3 +1,22 @@
 public class Goldmine {
+    if (total = 12){
+        
     
+
+        if (Turn == 0){
+            if (total == 3){
+                bankkonto1 = 650 + bankkonto1;
+            }
+            System.out.println("Du finder guld i bjergene og sælger det for 650 penge, Du er rig! Din saldo er nu: " + bankkonto1); 
+            
+        } else Turn = 1;
+
+        if (Turn == 1){
+            if (total == 3){
+                bankkonto2 = 650 + bankkonto2;
+            }
+            System.out.println("Du finder guld i bjergene og sælger det for 650 penge, Du er rig! Din saldo er nu: " + bankkonto2);
+        }else  Turn = 0;
+}
+}
 }
