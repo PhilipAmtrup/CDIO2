0d1233c91fd96719459124362e5afa0ce61e4390
PhilipAmtrup <s226870@dtu.dk>
2023-10-24 12:23:58 +0200

    spiller og konto

--- Player.java
+++ Player.java
@@ -1,13 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+// Spillernes score, tur og navn bliver sat i spil her
 public class Player {
+    public static void main(String[] args){
+   
 
-    Integer player1score = 0;
-    Integer player2score = 0;
+   Integer Turn = 0;
 
-    Integer turn = 0;
+   Scanner scanner = new Scanner(System.in);
+   Random random = new Random();
+
+   System.out.println("Velkommen til terningespillet!");
 
 
-    public class Konto {
-        int bankkonto1 = 1000;
-        int bankkonto2 = 1000;
-    }
+   System.out.print("Indtast spiller 1's navn: ");
+   String player1Name = scanner.nextLine();
+   System.out.print("Indtast spiller 2's navn: ");
+   String player2Name = scanner.nextLine();
+
 }
+
+   public class Konto {
+    public static void main (String[] args){
+
+    
+       int bankkonto1 = 1000;
+       int bankkonto2 = 1000;
+   }
+}
+}
+
--- terningespil.java
+++ terningespil.java
@@ -6,33 +6,35 @@ public class terningespil {
     
     public static void main(String[] args){
 
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+     
 
-        System.out.println("Velkommen til terningespillet!");
-
-
-        System.out.print("Indtast spiller 1's navn: ");
-        String player1Name = scanner.nextLine();
-        System.out.print("Indtast spiller 2's navn: ");
-        String player2Name = scanner.nextLine();
-
-
+        
         
     }
 
 
 
 
-    
+    // Spillernes score, tur og navn bliver sat i spil her
     public class Player {
+        public static void main(String[] args){
+       
+   
+       Integer Turn = 0;
+    
+       Scanner scanner = new Scanner(System.in);
+       Random random = new Random();
 
-       Integer player1score = 0;
-       Integer player2score = 0;
-   
-       Integer turn = 0;
-   
-   
+       System.out.println("Velkommen til terningespillet!");
+
+
+       System.out.print("Indtast spiller 1's navn: ");
+       String player1Name = scanner.nextLine();
+       System.out.print("Indtast spiller 2's navn: ");
+       String player2Name = scanner.nextLine();
+
+    }
+
        public class Konto {
            int bankkonto1 = 1000;
            int bankkonto2 = 1000;
