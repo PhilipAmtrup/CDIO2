388a3514bf681e8a905df85940ad2dbeb377fccf
Mo Zaabat <mzaabat@hotmail.com>
2023-10-24 12:18:56 +0200

    the pit

--- The_pit.java
+++ The_pit.java
@@ -1,3 +1,20 @@
 public class The_pit {
-    
+    if (total = 11){
+        
+
+        if (Turn == 0){
+            if (total == 11){
+                bankkonto1 = bankkonto1 - 50;
+            }
+            System.out.println("Din gambling afhængighed slår til igen og du taber 50 penge på en gladiatorkamp. Din saldo er nu: " + bankkonto1); 
+            
+        } else Turn = 1;
+
+        if (Turn == 1){
+            if (total == 11){
+                bankkonto2 = bankkonto2 - 50;
+            }
+            System.out.println("Din gambling afhængighed slår til igen og du taber 50 penge på en gladiatorkamp. Din saldo er nu: " + bankkonto2);
+        }else  Turn = 0;
+    }
 }
