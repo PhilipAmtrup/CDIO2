1e0575e32c4af3a0a8521c94d756fbd4bb62e86a
Mo Zaabat <mzaabat@hotmail.com>
2023-10-24 12:20:28 +0200

    goldmine

--- Goldmine.java
+++ Goldmine.java
@@ -1,3 +1,20 @@
 public class Goldmine {
-    
+    if (total = 12){
+        
+
+        if (Turn == 0){
+            if (total == 12){
+                bankkonto1 = bankkonto1 + 650;
+            }
+            System.out.println("Du finder guld i bjergene og sælger det, Du er rig! Din saldo er nu: " + bankkonto1); 
+            
+        } else Turn = 1;
+
+        if (Turn == 1){
+            if (total == 12){
+                bankkonto2 = bankkonto2 + 650;
+            }
+            System.out.println("Du finder guld i bjergene og sælger det, Du er rig! Din saldo er nu: " + bankkonto2);
+        }else  Turn = 0;
+    }
 }
