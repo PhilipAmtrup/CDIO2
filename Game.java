779e0686dc7140854fc22a91c293ce791b7a05f9
Mo Zaabat <mzaabat@hotmail.com>
2023-10-27 12:24:52 +0200

    sidste fejl fikset

--- Game.java
+++ Game.java
@@ -5,7 +5,7 @@ public class Game {
 
     Integer win = 3000;
 
-    public String[] felterne = {"Tower: Du møder en troldmand han fremtryller en blå safir frem, som du sælger for 250 penge.", "Crater: Du går igennem et krater og brænder dine fødder på varmt rumstøv du betaler 100 penge for at få behandling.", "Palace gates: Du stjæler en halskæde fra en prinsesse uden for paladsets indgang, og sælger den for 100 penge.", "Cold desert: Du befinder dig i en kold ørken og bliver nødt til at varme dig, en lokal sælger dig et varmt gedeskind for 20 penge.", "Walled city: Du erobre en by sammen med en rigtig stor oprørsbevægelse. Du modtager 180 penge for din brave indsats.", "Monastery: Du er utroligt træt og har brug for et sted at hvile dig. Du kommer til et kloster hvor nogle munke giver dig mad og ly, før du bevæger dig videre.", "Black cave: Du går ind i en mørk hule hvor du taber 70 penge til en gammel mand i et underligt, men meget dramatisk hjemmelavet brætspil.", "Huts in the mountain: Du går igennem en japansk landsby i bjergene og bliver udfordret til duel af en samurai, men du snyder og skyder ham med din revolver. Du tager hans 60 penge", "The Werewolfwall: Du kommer til vareulvevæggen og bliver bidt af en vareulv!. Du betaler en troldmand 80 penge for at løfte forbandelsen, men troldmanden er gavmild og giver dig en ekstra tur!", "The Pit: Din gambling afhængighed slår til igen og du taber 50 penge på en gladiatorkamp.", "Goldmine: Du finder noget meget værdigfuldt guld i bjergene og vælger at sælge det. Du får 650 penge guldet!!"};
+    public String[] felterne = {"Tower: Du moeder en troldmand han fremtryller en blaa safir frem, som du saelger for 250 penge.", "Crater: Du gaar igennem et krater og braender dine foedder paa varmt rumstoev du betaler 100 penge for at faa behandling.", "Palace gates: Du stjaeler en halskaede fra en prinsesse uden for paladsets indgang, og saelger den for 100 penge.", "Cold desert: Du befinder dig i en kold oerken og bliver noedt til at varme dig, en lokal saelger dig et varmt gedeskind for 20 penge.", "Walled city: Du erobre en by sammen med en rigtig stor oproersbevaegelse. Du modtager 180 penge for din brave indsats.", "Monastery: Du er utroligt traet og har brug for et sted at hvile dig. Du kommer til et kloster hvor nogle munke giver dig mad og ly, foer du bevaeger dig videre.", "Black cave: Du går ind i en moerk hule hvor du taber 70 penge til en gammel mand i et underligt, men meget dramatisk hjemmelavet braetspil.", "Huts in the mountain: Du går igennem en japansk landsby i bjergene og bliver udfordret til duel af en samurai, men du snyder og skyder ham med din revolver. Du tager hans 60 penge", "The Werewolfwall: Du kommer til vareulvevaeggen og bliver bidt af en vareulv!. Du betaler en troldmand 80 penge for at loefte forbandelsen, men troldmanden er gavmild og giver dig en ekstra tur!", "The Pit: Din gambling afhaengighed slaar til igen og du taber 50 penge paa en gladiatorkamp.", "Goldmine: Du finder noget meget vaerdigfuldt guld i bjergene og vaelger at saelge det. Du faar 650 penge guldet!!"};
     public Integer[] values = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
 
     Integer[] score = {1000, 1000};
@@ -36,12 +36,12 @@ public class Game {
             int scorechange2 = values[diceSum2 - 2];
 
                 if(score[0] <= 0){
-                System.out.println(player1Name + " din pengebeholdning er på " + score[0] + " og du har ikke flere penge tilbage og må desværre udgå spillet... Tillykke med sejren " + player2Name);
+                System.out.println(player1Name + " din pengebeholdning er paa " + score[0] + " og du har ikke flere penge tilbage og maa desværre udgaa spillet... Tillykke med sejren " + player2Name);
                 break;
                                 
                                 
                 } else if (score[1] <= 0){
-                System.out.println(player2Name + " din pengebeholdning er på " + score[1] + " og du har ikke flere penge tilbage og må desværre udgå spillet... Tillykke med sejren " + player1Name);
+                System.out.println(player2Name + " din pengebeholdning er paa " + score[1] + " og du har ikke flere penge tilbage og maa desværre udgaa spillet... Tillykke med sejren " + player1Name);
                 break;
                                 
                 } 
@@ -52,7 +52,7 @@ public class Game {
             scanner.nextLine();
             
 
-            System.out.println(player1Name + " du rullede " + diceSum + " og har landet på " + felter);
+            System.out.println(player1Name + " du rullede " + diceSum + " og har landet paa " + felter);
             score[turn[0]] += scorechange;
 
 
@@ -62,7 +62,7 @@ public class Game {
 
             scanner.nextLine();
 
-            System.out.println(player2Name + " du rullede " + diceSum2 + " og har landet på " + felter2);
+            System.out.println(player2Name + " du rullede " + diceSum2 + " og har landet paa " + felter2);
             score[turn[1]] += scorechange2;
 
             scanner.nextLine();
